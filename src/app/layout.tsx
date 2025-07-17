db8fa09bcbe228f3f71bcbf8e54c915e4b201f29
Jephthah Dominic <stdominicjephthah63@gmail.com>
2025-07-17 02:14:22 +0200

    next app setup

---
+++ src/app/layout.tsx
@@ -0,0 +1,49 @@
+import type { Metadata } from "next";
+import localFont from "next/font/local";
+import "./globals.css";
+
+const satoshiFont = localFont({
+  src: [
+    {
+      path: "/fonts/satoshi-cdnfonts/Satoshi-Regular.otf",
+      weight: "400",
+      style: "normal",
+    },
+    {
+      path: "/fonts/satoshi-cdnfonts/Satoshi-Medium.otf",
+      weight: "500",
+      style: "medium",
+    },
+    {
+      path: "/fonts/satoshi-cdnfonts/Satoshi-Italic.otf",
+      weight: "500",
+      style: "medium",
+    },
+    {
+      path: "/fonts/satoshi-cdnfonts/Satoshi-Bold.otf",
+      weight: "500",
+      style: "medium",
+    },
+  ],
+});
+
+export const metadata: Metadata = {
+  title: "Ministry of Finance",
+  description: "Ministry of Finance",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={`antialiased ${satoshiFont.className}`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
