56fb78c9a4bc41a36f619384f83c0cc2f543332a
Jephthah Dominic <stdominicjephthah63@gmail.com>
2025-07-18 11:11:03 +0200

    Working on the about us section of the home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,32 @@
 import { AppLink } from "@/components/AppLink";
 import { Hero } from "@/components/Hero";
+import { Title } from "@/components/Title";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="h-screen w-screen">
+    <div className="h-screen">
       <Hero />
+      <section className="h-[85vh] px-[3rem] bg-[url('/images/gradient.png')] bg-cover bg-center bg-no-repeat flex flex-col items-center justify-center">
+        <div className="w-full flex flex-[1fr 1fr] justify-center items-center gap-20">
+          <div className="flex flex-col gap-2">
+            <Title label="About Us"/>
+            <h2 className="text-white text-[43px] font-bold">Our Mandate – <br/>Ministry of Finance</h2>
+          </div>
+          <div className="max-w-2/4 flex flex-col gap-8">
+            <p className="text-white text-[16px] leading-relaxed text-justify">
+              Established in 2022, the Imo State Ministry of Digital Economy and E-Government (IMDEEG) 
+              is committed to accelerating digital transformation across the state. We build a digitally 
+              empowered Imo through inclusive innovation, cutting-edge technology, and people-centered 
+              digital governance.
+            </p>
+            <div>
+              <AppLink href="/about-us" label="Learn More" variant="secondary" className="border border-1 border-primary-green text-[15px] px-[2rem] py-[12px] rounded-[3.4px] font-medium"/>
+            </div>
+          </div>
+
+        </div>
+      </section>
     </div>
   );
 }
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,12 +2,12 @@ import { Title } from "./Title";
 
 export const Hero = () => {
     return(
-        <div className="relative h-[85vh] px-[3rem] w-screen flex flex-col justify-center bg-[url('/images/heroImage.png')] bg-cover bg-center">
+        <div className="relative h-[85vh]  px-[3rem] flex flex-col justify-center bg-[url('/images/heroImage.png')] bg-cover bg-center">
             <div className="absolute inset-0 bg-gradient-to-b from-black/70 via-black/40 to-transparent z-0"></div>
             <div className="relative z-10 py-10">
                 <div className="w-[60%] flex flex-col gap-4">
                     <Title label="Imo State Ministry of Finance"/>
-                    <h1 className="text-[2.5rem] font-bold text-white">Transparent Governance. Accountable Finance. Sustainable Growth.</h1>
+                    <h1 className="text-[60px] font-bold text-white">Transparent Governance, Accountable Finance, Sustainable Growth.</h1>
                     <p className="text-[1rem] font-light leading-[1.5] text-white">
                         The Imo State Ministry of Finance is committed to managing public funds with transparency, efficiency, 
                         and accountability. We drive fiscal policy, budget planning, 
